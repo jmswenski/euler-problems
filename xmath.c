df714ba3928dac496d62a12f73afc59e1e91e36e
Jason Swenski <jswenski@gmail.com>
2014-08-18 23:37:42 -0600

    Fixing decl

--- xmath.c
+++ xmath.c
@@ -1,11 +1,12 @@
 #include "xmath.h"
 
 ulong64 gcd(ulong64 a, ulong64 b) {
-  long c;
-  while (a != 0) {
-     c = a;
-     a = b % a;
-     b = c;
-  }
-  return b;
+
+    ulong64 c;
+    while (a != 0) {
+        c = a;
+        a = b % a;
+        b = c;
+     }
+     return b;
 }
